c73a9d20d26915fba5aaa1c9dbc2cae87ced6466
GAURAV KADAM <gskadam3b@gmail.com>
2026-01-13 23:39:40 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -273,9 +273,14 @@ export default function LoginPage() {
                     </div>
                 </div>
 
-               
-                <div className="mt-8 text-center text-sm text-gray-500">
-                    <p>🔒 Secured with industry-standard encryption</p>
+                {/* Security Badge */}
+                <div className="mt-8 flex justify-center">
+                    <div className="auth-security-badge">
+                        <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                            <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                        </svg>
+                        Secured with industry-standard encryption
+                    </div>
                 </div>
             </div>
         </div>
